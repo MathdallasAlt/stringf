127b890398eb6b0fd3e67e3ab1072a4a527e3463
Jaiwant Morampudi <jaimorampudi164@gmail.com>
2024-03-08 11:42:22 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,20 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-
 VERSION = '0.1'
-DESCRIPTION = 'Functions for strings.'
-LONG_DESCRIPTION = 'A package that includes various functions for strings.'
+DESCRIPTION = 'String functions'
+LONG_DESCRIPTION = 'A package that has various functions for strings.'
 
 # Setting up
 setup(
     name="stringf",
     version=VERSION,
-    author="Mathdallas_code(Jaiwant morampudi)",
+    author="Mathdallas_code(Jaiwant Morampudi)",
     author_email="<krishnapacs255@gmail.com>",
     description=DESCRIPTION,
-    long_description_content_type="text/markdown",
     packages=find_packages(),
-    keywords=['python','string','functions'],
+    install_requires=[],
+    keywords=['python', 'string', 'functions'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",

d3b577995a110d9a052a67f2cffe2569ada7c61a
Jaiwant Morampudi <krishnapacs255@gmail.com>
2024-03-08 19:07:27 +0530

    Added description

--- setup.py
+++ setup.py
@@ -2,9 +2,27 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Functions for strings'
-LONG_DESCRIPTION = 'A package that has several functions to work with strings and English words.'
+LONG_DESCRIPTION = '''
+A package that has several functions to work with strings and English words.
+Functions-
+
+plural-Converts any wprd to plural form.
+e.g. plural("python")-"pythons"
+
+list_to_str: Convert a list into a string e.g. ['p','y','t','h','o','n']-'python'
+
+join: joins two words e.g. 'pyt','hon'-'python'
+
+add: Like the join() functions but can join word after the letter at the specified position
+e.g. add("bore",1,"ef")-'before'
+
+reverse: Reverses a string 
+e.g. reverse("python")-"nohtyp"
+
+More functions coming soon!
+'''
 
 # Setting up
 setup(
@@ -13,6 +31,7 @@ setup(
     author="Mathdallas_code(Jaiwant Morampudi)",
     author_email="<krishnapacs255@gmail.com>",
     description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=[],
     keywords=['python','string','English','words','functions'],
--- stringf/__init__.py
+++ stringf/__init__.py
@@ -1,1 +1,2 @@
 from stringf import *
+
